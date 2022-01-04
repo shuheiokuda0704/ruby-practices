e498ef444bb9dc6ae0e290a4c9adac3041c6fda3
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-04 22:57:10 +0900

    Initial ls

---
+++ 05.ls/ls.rb
@@ -0,0 +1,46 @@
+# fronzen_string_literal: true
+
+class Ls
+  MAX_COLUMN_NUM = 3
+  COLUMN_MARGIN  = 2
+
+  def execute
+    get_directory_items
+    put_directory_items
+  end
+
+  def get_directory_items
+    @items = []
+    @max_item_name_length = 0
+
+    Dir.foreach('..') do |item|
+      @items.append(item) unless item =~ /^\./
+      if @max_item_name_length < item.length
+        @max_item_name_length = item.length
+      end
+    end
+
+    @items.sort!
+  end
+
+  def put_directory_items
+    row_no = ((@items.length - 1) / MAX_COLUMN_NUM) + 1
+    (0..(row_no - 1)).each do |row|
+      (0..(MAX_COLUMN_NUM - 1)).each do |column|
+        index = MAX_COLUMN_NUM * row + column
+
+        print @items[index]
+        break if index == (@items.length - 1)
+
+        if column != (MAX_COLUMN_NUM - 1)
+          print ' ' * (@max_item_name_length - @items[index].length + COLUMN_MARGIN)
+        end
+      end
+
+      puts ''
+    end
+  end
+end
+
+ls = Ls.new
+ls.execute
