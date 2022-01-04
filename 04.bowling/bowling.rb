9d6e1afd9923e07e4318239bf4d7db315fe146a0
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-04 16:14:23 +0900

    Add initial commit

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Bowling
+  def initialize(input:)
+    @inputs = input
+  end
+
+  def score
+    parse_inputs
+    @score = calculate
+    @score
+  end
+
+  private
+
+  def parse_inputs
+    @inputs_array = @inputs.split(',')
+    @inputs_array.map! do |i|
+      if i.eql?('X')
+        10
+      else
+        i.to_i
+      end
+    end
+  end
+
+  def calculate
+    score = 0
+    frame = 1
+    first_throw = true
+    @inputs_array.each_with_index do |value, index|
+      score += value
+
+      if frame != 10
+        if first_throw && value == 10
+          # strike
+          frame += 1
+
+          # Next 2 throws are added as additional points
+          score = score + @inputs_array[index+1] + @inputs_array[index+2]
+        elsif !first_throw && (value + @inputs_array[index-1]) == 10
+          # spare
+          first_throw = !first_throw
+          frame += 1
+
+          # Next throw is added as additional point
+          score = score + @inputs_array[index+1]
+        else
+          frame += 1 unless first_throw
+          first_throw = !first_throw
+        end
+      end
+    end
+
+    score
+  end
+end
+
+bowling = Bowling.new(input: ARGV[0])
+p bowling.score
---
+++ 04.bowling/spec/bowling_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../bowling'
+
+RSpec.describe Bowling do
+  describe '#score' do
+
+    context 'No arguments' do
+      example 'Argument Error' do
+        expect { Bowling.new }.to raise_error
+      end
+    end
+
+    context 'Invalid arguments' do
+      example 'score is 0' do
+        bowling = Bowling.new(input: 'ABC')
+        expect(bowling.score).to eq 0
+      end
+    end
+
+    context 'Correct arguments' do
+      example 'Perfect (300)' do
+        bowling = Bowling.new(input: 'X,X,X,X,X,X,X,X,X,X,X,X')
+        expect(bowling.score).to eq 300
+      end
+
+      example '0' do
+        bowling = Bowling.new(input: '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        expect(bowling.score).to eq 0
+      end
+
+      example 'SampleA input: 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5 score: 139' do
+        bowling = Bowling.new(input: '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+        expect(bowling.score).to eq 139
+      end
+
+      example 'SampleB input: 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X score: 164' do
+        bowling = Bowling.new(input: '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+        expect(bowling.score).to eq 164
+      end
+
+      example 'SampleC input: 0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4 score: 107' do
+        bowling = Bowling.new(input: '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+        expect(bowling.score).to eq 107
+
+      end
+
+      example 'SampleD input: 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0 score: 134' do
+        bowling = Bowling.new(input: '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+        expect(bowling.score).to eq 134
+        
+      end
+
+      example 'SampleE input: 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8 score: 144' do
+        bowling = Bowling.new(input: '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+        expect(bowling.score).to eq 144
+        
+      end
+    end
+  end
+end
