3ac69063bebf053f8a171d747d8da7eb60fb143e
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-04 23:02:10 +0900

    Fix rubocop detected issues

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,15 +1,15 @@
-# fronzen_string_literal: true
+# frozen_string_literal: true
 
 class Ls
   MAX_COLUMN_NUM = 3
   COLUMN_MARGIN  = 2
 
   def execute
-    get_directory_items
-    put_directory_items
+    parse_directory_items
+    render_directory_items
   end
 
-  def get_directory_items
+  def parse_directory_items
     @items = []
     @max_item_name_length = 0
 
@@ -21,7 +21,7 @@ class Ls
     @items.sort!
   end
 
-  def put_directory_items
+  def render_directory_items
     row_no = ((@items.length - 1) / MAX_COLUMN_NUM) + 1
     (0..(row_no - 1)).each do |row|
       (0..(MAX_COLUMN_NUM - 1)).each do |column|
@@ -30,7 +30,8 @@ class Ls
         print @items[index]
         break if index == (@items.length - 1)
         next if column == (MAX_COLUMN_NUM - 1)
-        print ' ' * (@max_item_name_length - @items[index].length + COLUMN_MARGIN) 
+
+        print ' ' * (@max_item_name_length - @items[index].length + COLUMN_MARGIN)
       end
 
       puts ''
