08f26e73af0f9c9833cba0684de55a89a008bbfa
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-07 22:32:41 +0900

    Refactor unnecessary instance variables to local variables

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,19 +2,18 @@
 
 class Bowling
   def initialize(input:)
-    @inputs = input
+    @inputs = parse_inputs(input)
   end
 
   def score
-    parse_inputs
-    @score = calculate
+    calculate
   end
 
   private
 
-  def parse_inputs
-    @inputs_array = @inputs.split(',')
-    @inputs_array.map! do |i|
+  def parse_inputs(input)
+    inputs = input.split(',')
+    inputs.map do |i|
       if i.eql?('X')
         10
       else
@@ -24,29 +23,29 @@ class Bowling
   end
 
   def calculate
-    @score = 0
+    score = 0
     frame = 1
     first_throw = true
 
-    @inputs_array.each_with_index do |value, index|
-      add_score(value)
+    @inputs.each_with_index do |value, index|
+      score += value
 
       next if frame == 10
 
       if strike?(first_throw, value)
         frame += 1
-        add_score(@inputs_array[index + 1], @inputs_array[index + 2])
+        score += @inputs[(index + 1)..(index + 2)].sum
       elsif spare?(first_throw, index)
         first_throw = true
         frame += 1
-        add_score(@inputs_array[index + 1])
+        score += @inputs[index + 1]
       else
         frame += 1 unless first_throw
         first_throw = !first_throw
       end
     end
 
-    @score
+    score
   end
 
   def strike?(first_throw, value)
@@ -54,11 +53,7 @@ class Bowling
   end
 
   def spare?(first_throw, index)
-    !first_throw && (@inputs_array[(index - 1)..index]).sum == 10
-  end
-
-  def add_score(*values)
-    @score += values.sum
+    !first_throw && (@inputs[(index - 1)..index]).sum == 10
   end
 end
 
