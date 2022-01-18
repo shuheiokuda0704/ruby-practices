e4e2b479d5a8c24b601a95b3fb308c756d10a459
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-18 22:04:20 +0900

    format mode, nlink, and uname

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'etc'
+
 class Ls
   MAX_COLUMN_NUM = 3
   COLUMN_MARGIN  = 7
@@ -48,26 +50,36 @@ class Ls
       end
     else
       blocks = 0
+      max_nlink_digits = @items.map { |item| File.lstat("#{@target_dir + item}").nlink.to_s.length }.max
       @items.each do |item|
         item_stat = File.lstat("#{@target_dir + item}")
-        puts "#{parse_mode(item_stat.mode.to_s(8))} #{item_stat.nlink} #{item_stat.uid} #{item_stat.gid} #{item_stat.size} #{item_stat.atime} #{item}"
+        puts "#{format_mode(item_stat.mode.to_s(8))}  #{format_nlink(item_stat.nlink, max_nlink_digits)}  #{format_uname(item_stat.uid)} #{item_stat.gid} #{item_stat.size} #{item_stat.atime} #{item}"
         blocks += item_stat.blocks
       end
       puts "Blocks: #{blocks}"
     end
   end
 
-  def parse_mode(mode)
-    type_map = {'01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's'}
-    formatted_mode = format("%06<mode>d", mode: mode)
-    type_num = formatted_mode.slice(..1)
-    parsed_mode = +type_map[type_num]
-    formatted_mode.slice(3..).each_char do |c|
-      parsed_mode.concat(((c.to_i >> 2) % 2) == 1 ? 'r' : '-')
-      parsed_mode.concat(((c.to_i >> 1) % 2) == 1 ? 'w' : '-')
-      parsed_mode.concat(((c.to_i >> 0) % 2) == 1 ? 'x' : '-')
+  def format_mode(mode)
+    type_map = {'01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b',
+                '10' => '-', '12' => 'l', '14' => 's'}
+    aligned_mode = format("%06<mode>d", mode: mode)
+    type_num = aligned_mode.slice(..1)
+    formatted_mode = +type_map[type_num]
+    aligned_mode.slice(3..).each_char do |c|
+      formatted_mode.concat(((c.to_i >> 2) % 2) == 1 ? 'r' : '-')
+      formatted_mode.concat(((c.to_i >> 1) % 2) == 1 ? 'w' : '-')
+      formatted_mode.concat(((c.to_i >> 0) % 2) == 1 ? 'x' : '-')
     end
-    parsed_mode
+    formatted_mode
+  end
+
+  def format_nlink(nlink, max_nlink_digits)
+    format("%#{max_nlink_digits}d", nlink)
+  end
+
+  def format_uname(uid)
+    Etc.getpwuid(uid).name
   end
 end
 
